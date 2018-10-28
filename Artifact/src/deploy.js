bd35fe576e70768d3c66e8ef3678d7be5136c422
Nolics74 <nikch3008@mail.ru>
2018-10-28 14:56:38 +0700

    Update Artifact/src/deploy.js

--- Artifact/src/deploy.js
+++ Artifact/src/deploy.js
@@ -27,7 +27,7 @@ function buildStages(){
   })
 
   deployButton.classList.add("deploy-btn", "btn" ,"UI" ,"display-none")
-  deployButton.textContent = "Deploys"
+  deployButton.textContent = "Deplo"
   game.div.appendChild(deployButton);
   deployButton.addEventListener("click",function(){
     if (stages[0].hasChildNodes() || stages[1].hasChildNodes()){
