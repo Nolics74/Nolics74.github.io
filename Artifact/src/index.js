f40e32b3ba122d87a1ae69f031405a775db15ed8
Nolics74 <nikch3008@mail.ru>
2018-10-31 17:34:55 +0700

    Update Artifact/src/index.js

--- Artifact/src/index.js
+++ Artifact/src/index.js
@@ -102,8 +102,8 @@ const game = (() => {
   // const bottomPassButton = document.getElementById("pass-btn-bottom");
   // const topPassButton = document.getElementById("pass-btn-top");
   let deck = ["Conflagration","Call the Reserves", "Better Late Than Never","Iron Branch Protection","Avernus' Blessing","Dimensional Portal","Bronze Legionnaire","Marrowfell Brawler","Ogre Conscript","Troll Soothsayer","Untested Grunt","Roseleaf Wall","Thunderhide Alpha"]
-  deck = deck.concat(deck,deck)
-  let players = [player(0,"Radiant",
+  deck = deck.concat(deck,deck)//Radiant
+  let players = [player(0,"Силы света",
                     ["Keefe the Bold","Fahrvhan the Dreamer","J\'Muy the Wise","Debbi the Cunning","Crystal Maiden"],//Axe
                     deck.slice()),
                  player(1,"Dire",
